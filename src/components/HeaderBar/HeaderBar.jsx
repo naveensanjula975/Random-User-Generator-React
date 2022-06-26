eb249f709b208bc09ecc14e25ee634a968c46c5b
naveen sanjula <naveensanjula975@gmail.com>
2022-06-26 16:22:25 +0530

    navbar

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeaderBar() {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-light bg-light shadow-sm">
             <span className="navbar-brand mb-0 h1">Name List</span>
         </nav>
     )
