421dce9c014dba0f400302c2cdd4baa7aef967ab
naveen sanjula <naveensanjula975@gmail.com>
2022-06-26 14:29:02 +0530

    details random

--- src/components/NameList/NameList.jsx
+++ src/components/NameList/NameList.jsx
@@ -3,40 +3,59 @@ import NameListItem from "./NameListItem";
 
 function NameList() {
 
-    const nameList = {
-        name: {
-            title: "mr",
-            first: "brad",
-            last: "gibson"
-        },
-        location: {
-            city: "kilcoole",
-        },
-        email: "brad.gibson@example.com",
-        dob: {
-            date: "1993-07-20T09:44:18.674Z",
-            age: 26
-        },
-        picture: {
-            medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-        },
-    };
+  const nameList = [
+    {
+      "name": { "title": "mr", "first": "brad", "last": "gibson" },
+      "location": { "city": "kilcoole" },
+      "email": "brad.gibson@example.com",
+      "dob": { "date": "1993-07-20T09:44:18.674Z", "age": 26 },
+      "picture": { "medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
+    },
+    {
+      "name": { "title": "mr", "first": "brad", "last": "gibson" },
+      "location": { "city": "kilcoole", },
+      "email": "brad.gibson@example.com",
+      "dob": { "date": "1993-07-20T09:44:18.674Z", "age": 26 },
+      "picture": { "medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
+    },
+    {
+      "name": { "title": "mr", "first": "brad", "last": "gibson" },
+      "location": { "city": "kilcoole", },
+      "email": "brad.gibson@example.com",
+      "dob": { "date": "1993-07-20T09:44:18.674Z", "age": 26 },
+      "picture": { "medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
+    }
+  ];
 
-    return (
-        <div>
-            <h1>Name List</h1>
-            <hr />
-            <ul>
-                <NameListItem
-                    name={`${nameList.name.first} ${nameList.name.last}`}
-                    city={nameList.location.city}
-                    email={nameList.email}
-                    birthday={nameList.dob.date}
-                    avatar={nameList.picture.medium}
-                />
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Name List</h1>
+      <hr />
+      <ul>
+        <NameListItem
+          name={`${nameList[0].name.first} ${nameList[0].name.last}`}
+          city={nameList[0].location.city}
+          email={nameList[0].email}
+          birthday={nameList[0].dob.date}
+          avatar={nameList[0].picture.medium}
+        />
+        <NameListItem
+          name={`${nameList[1].name.first} ${nameList[1].name.last}`}
+          city={nameList[1].location.city}
+          email={nameList[1].email}
+          birthday={nameList[1].dob.date}
+          avatar={nameList[1].picture.medium}
+        />
+        <NameListItem
+          name={`${nameList[2].name.first} ${nameList[2].name.last}`}
+          city={nameList[2].location.city}
+          email={nameList[2].email}
+          birthday={nameList[2].dob.date}
+          avatar={nameList[2].picture.medium}
+        />
+      </ul>
+    </div>
+  );
 }
 
 export default NameList;
