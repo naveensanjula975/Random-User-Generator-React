7eec65769b2944f17a6b2a3c9ff8d9e5cb1325da
naveen sanjula <naveensanjula975@gmail.com>
2022-06-26 15:27:08 +0530

    nameListComponent

--- src/components/NameList/NameList.jsx
+++ src/components/NameList/NameList.jsx
@@ -24,36 +24,28 @@ function NameList() {
       "email": "brad.gibson@example.com",
       "dob": { "date": "1993-07-20T09:44:18.674Z", "age": 26 },
       "picture": { "medium": "https://randomuser.me/api/portraits/med/men/75.jpg", },
-    }
+    },
   ];
+
+  const nameListComponent = () => {
+    return nameList.map((aName) => {
+      return (
+        <NameListItem
+          name={`${aName.name.first} ${aName.name.last}`}
+          city={aName.location.city}
+          email={aName.email}
+          birthday={aName.dob.date}
+          avatar={aName.picture.medium}
+        />
+      );
+    });
+  };
 
   return (
     <div>
       <h1>Name List</h1>
       <hr />
-      <ul>
-        <NameListItem
-          name={`${nameList[0].name.first} ${nameList[0].name.last}`}
-          city={nameList[0].location.city}
-          email={nameList[0].email}
-          birthday={nameList[0].dob.date}
-          avatar={nameList[0].picture.medium}
-        />
-        <NameListItem
-          name={`${nameList[1].name.first} ${nameList[1].name.last}`}
-          city={nameList[1].location.city}
-          email={nameList[1].email}
-          birthday={nameList[1].dob.date}
-          avatar={nameList[1].picture.medium}
-        />
-        <NameListItem
-          name={`${nameList[2].name.first} ${nameList[2].name.last}`}
-          city={nameList[2].location.city}
-          email={nameList[2].email}
-          birthday={nameList[2].dob.date}
-          avatar={nameList[2].picture.medium}
-        />
-      </ul>
+      <ul>{nameListComponent()}</ul>
     </div>
   );
 }
