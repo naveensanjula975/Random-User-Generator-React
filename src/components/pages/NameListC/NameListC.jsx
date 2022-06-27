910e740e3bb1fa6474653c244d5a3941a4b68442
naveen sanjula <naveensanjula975@gmail.com>
2022-06-27 16:36:41 +0530

    addNameHandler

--- src/components/pages/NameListC/NameListC.jsx
+++ src/components/pages/NameListC/NameListC.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
 class NameListC extends Component {
+    constructor(props) {
+        super(props);
+        console.log('Constructor Called');
+        this.state = { message: 'Hello State' };
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount Method Called');
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate Method Called');
+    }
+
+    addNameHandler = () => {
+        console.log('Add name button clicked');
+        console.log(this.state.message);
+    }
 
     render() {
         return (
             <div className="container mt-4">
-                <button className="btn btn-primary mb-2">Add Name</button>
+                <button className="btn btn-primary mb-2" onClick={this.addNameHandler} >
+                    Add Name
+                </button>
                 <ul className="list-group"></ul>
             </div>
         );
